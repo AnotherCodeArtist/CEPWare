b143c4df5854d002d422e737a9a6528ab8f73aee
Sebastian Weiland <hello@sweiland.at>
2018-10-22 15:18:57 +0200

    finally get msgs to mosquitto from nodered

--- nodered-script.js
+++ nodered-script.js
@@ -1,12 +1,12 @@
 var jsonCont = msg.payload;
 
-var count = context.get('count')||0;
+var count = global.get('count')||0;
 count++;
-context.set('count',count);
+global.set('count',count);
 
 jsonCont.id = 1+count;
 jsonCont.temperature.value.degrees = Math.floor(Math.random()*(50-18)+18);
 msg.payload = jsonCont;
 node.warn(jsonCont.temperature.value.degrees);
-msg.topic = jsonCont.type + " " + jsonCont.id;
+msg.topic = jsonCont.type + "/" + jsonCont.id;
 return msg;
