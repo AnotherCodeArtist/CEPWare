91b9899c3ccc0a304b006db60ce96d0a4567a6e4
Gregor Fernbach <gregor.fernbach.2@mailbox.org>
2018-11-04 19:10:55 +0100

    Updating the requests and adding temporary setup information

---
+++ ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink
+
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.fiware.cosmos.orion.flink.connector.{OrionSource}
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * For a tutorial how to write a Flink streaming application, check the
+ * tutorials and examples on the <a href="http://flink.apache.org/docs/stable/">Flink Website</a>.
+ *
+ * To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+object StreamingJob {
+  def main(args: Array[String]) {
+    // set up the streaming execution environment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val eventStream = env.addSource(new OrionSource(9001))
+
+    val processedDataStream = eventStream
+      .flatMap(event => event.entities)
+      .map(entity => {
+        val temp = entity.attrs("temperature").value.asInstanceOf[Number].floatValue()
+        new Temp_Node(entity.id, temp)
+      })
+      .keyBy("id")
+      .timeWindow(Time.seconds(5), Time.seconds(2))
+      .min("temperature")
+
+    // Print the results with a single thread, rather than in parallel
+    processedDataStream.print().setParallelism(1)
+    /*
+     * Here, you can start creating your execution plan for Flink.
+     *
+     * Start with getting some data from the environment, like
+     *  env.readTextFile(textPath);
+     *
+     * then, transform the resulting DataStream[String] using operations
+     * like
+     *   .filter()
+     *   .flatMap()
+     *   .join()
+     *   .group()
+     *
+     * and many more.
+     * Have a look at the programming guide:
+     *
+     * http://flink.apache.org/docs/latest/apis/streaming/index.html
+     *
+     */
+
+    // execute program
+    env.execute("Socket Window NgsiEvent")
+  }
+  case class Temp_Node(id: String, temperature: Float)
+}
