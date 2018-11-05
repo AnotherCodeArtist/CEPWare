e7a2a9d8b42e82f6efd3ca4912a15a956a701dac
Sebastian Weiland <hello@sweiland.at>
2018-11-05 16:06:41 +0100

    rudimentary implementation of #7

--- Scripts/nodered-script.js
+++ Scripts/nodered-script.js
@@ -1,11 +1,13 @@
-var jsonCont = msg.payload;
-
 var count = context.get('count')||0;
 count++;
 context.set('count',count);
-
-jsonCont.id = 1+count;
-jsonCont.temperature.value.degrees = Math.floor(Math.random()*(50-18)+18);
-msg.payload = jsonCont;
-node.warn(jsonCont.temperature.value.degrees);
+if (count <= 20) {msg.payload = "t|20";}
+else if (count <= 50) {msg.payload = "t|" + count;}
+else {msg.payload = "t|" + Math.floor(Math.random()*(90-30)+30);}
+msg.url = "http://172.17.0.8:7896/iot/d?k=4jggokgpepnvsb2uv4s40d59ov&i=IoT-R" + Math.floor(Math.random()*(6-1)+1);
+msg.headers = {};
+msg.headers['Fiware-Service'] = 'cepware';
+msg.headers['Fiware-ServicePath'] = '/rooms';
+msg.headers['Content-Type'] = 'text/plain';
+node.warn(msg.payload);
 return msg;
--- nodered-script-new.js
+++
@@ -1,9 +0,0 @@
-msg.payload = "t|" + Math.floor(Math.random()*(50-18)+18);
-msg.url = "http://172.17.0.8:7896/iot/d?k=4jggokgpepnvsb2uv4s40d59ov&i=IoT-R" + Math.floor(Math.random()*(6-1)+1);
-msg.headers = {};
-msg.headers['Fiware-Service'] = 'cepware';
-msg.headers['Fiware-ServicePath'] = '/rooms';
-msg.headers['Content-Type'] = 'text/plain';
-node.warn(msg.payload);
-node.warn(msg.url);
-return msg;
