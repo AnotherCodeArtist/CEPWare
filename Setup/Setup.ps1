ceebdd9d5ce0de80bf083ddeb28006fee261dbd1
Gregor Fernbach <gregor.fernbach.2@mailbox.org>
2018-11-14 16:19:28 +0100

    Correcting the setup and Postman requests

--- Setup/Setup.ps1
+++ Setup/Setup.ps1
@@ -1,12 +1,8 @@
-Write-Host "Welcome to CEPWARE. This is the automated setup. Please follow the instructions and be patient until everything is setup"
+Write-Host "Welcome to CEPWARE. This is the automated setup. Before you run this script. Please do a docker-compose up. `n Please follow the instructions and be patient until everything is setup"
 $stw= Read-Host -Prompt "Please Enter the Full Path to your CEPWARE folder (e.g. C:\Workspace\CEPWARE, here we need C:\Workspace)"
 $compose =('\CEPWARE')
 $jar= ('\CEPWARE\ApacheFlink\flink-quickstart-scala\target\')
 $jarFile= ('flink-quickstart-scala-1.0-SNAPSHOT.jar')
-$jm=(docker inspect -f '{{.Id}}' jobmanager)
-cd $stw$compose
-docker-compose up -d
-Start-Sleep -s 120
 $jm=(docker inspect -f '{{.Id}}' jobmanager)
 docker cp $stw$jar$jarFile $jm`:/opt/flink/$jarFile
 docker exec -d -t -i $jm flink run /opt/flink/$jarFile
