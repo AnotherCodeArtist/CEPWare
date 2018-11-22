7099ed974f28101a6ddd9a5a8f31e8cdb22ec035
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-11-22 13:31:14 +0100

    WP6: Sensor Timeout implemented

--- ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
+++ ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
@@ -24,9 +24,10 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.fiware.cosmos.orion.flink.connector.{NgsiEvent, OrionSource}
 import java.io._
 import java.lang
+import java.util.Calendar
 import java.util.stream.Collectors._
-import scala.io.Source
 
+import scala.io.Source
 import org.apache.flink.api.common.functions.AggregateFunction
 import org.apache.flink.api.java.tuple.Tuple
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction
@@ -49,23 +50,50 @@ import scala.collection.TraversableOnce
 
 object StreamingJob {
 
+  var timerR1: Long = 0
+  var timerR2: Long = 0
+  var timerR3: Long = 0
+  var timerR4: Long = 0
+  var timerR5: Long = 0
+
   def main(args: Array[String]) {
 
+    // function to check if temperature is too hot or too cold
     def checkTemp(tuple: roomTemp): String = {
-      var warning: String = tuple.id
-      val temperature: String = "temperature: " + tuple.temp
+      var message: String = ""
+      val currentTime = System.currentTimeMillis() / 1000
+
+      tuple.id match {
+        case "R1" => timerR1 = System.currentTimeMillis() / 1000
+        case "R2" => timerR2 = System.currentTimeMillis() / 1000
+        case "R3" => timerR3 = System.currentTimeMillis() / 1000
+        case "R4" => timerR4 = System.currentTimeMillis() / 1000
+        case "R5" => timerR5 = System.currentTimeMillis() / 1000
+      }
+
+      if (timerR1 != 0 && currentTime - timerR1 > 5){
+        message = message + "R1: timeout! please check if the sensor is working!" + "\n"
+      }
+      if (timerR2 != 0 && currentTime - timerR2 > 5){
+        message = message + "R2: timeout! please check if the sensor is working!" + "\n"
+      }
+      if (timerR3 != 0 && currentTime - timerR3 > 5){
+        message = message + "R3: timeout! please check if the sensor is working!" + "\n"
+      }
+      if (timerR4 != 0 && currentTime - timerR4 > 5){
+        message = message + "R4: timeout! please check if the sensor is working!" + "\n"
+      }
+      if (timerR5 != 0 && currentTime - timerR5 > 5){
+        message = message + "R5: timeout! please check if the sensor is working!" + "\n"
+      }
 
       if (tuple.temp.toFloat > 60f) {
-        warning = warning + ": room on fire!"
-        warning + " " + temperature + "°C"
+        message = message + tuple.id + ": room on fire!" + " temperature: " + tuple.temp + "°C" + "\n"
       }
-      else if (tuple.temp.toFloat < 15f) {
-        warning = warning + ": do not forget to close the windows!"
-        warning + " " + temperature + "°C"
+      if (tuple.temp.toFloat < 15f) {
+        message = message + tuple.id + ": do not forget to close the windows!" + " temperature: " + tuple.temp + "°C" + "\n"
       }
-      else {
-        ""
-      }
+      message
     }
 
     // set up the streaming execution environment
@@ -78,10 +106,9 @@ object StreamingJob {
     val warningStream = processedDataStream.map(checkTemp(_))
     val filteredWarningStream = warningStream.filter(_ != "")
 
-    /*
-    Writing the Results in a log.txt File
-    Attention: log.txt must not exist before the task is executed
-    */
+
+    // Writing the Results in a log.txt File
+    // Attention: log.txt must not exist before the task is executed
     filteredWarningStream.writeAsText("/tmp/log.txt")
 
     // execute program
