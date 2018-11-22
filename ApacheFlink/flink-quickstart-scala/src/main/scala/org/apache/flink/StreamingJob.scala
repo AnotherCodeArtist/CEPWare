b0411519e2cc03a14d0941f19e988d3a78840309
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-11-22 15:51:46 +0100

    WP6: Implemented rising temperature function

--- ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
+++ ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
@@ -56,6 +56,14 @@ object StreamingJob {
   var timerR4: Long = 0
   var timerR5: Long = 0
 
+  var wipeTimer: Long = System.currentTimeMillis() / 1000
+
+  var listR1: List[Float] = List()
+  var listR2: List[Float] = List()
+  var listR3: List[Float] = List()
+  var listR4: List[Float] = List()
+  var listR5: List[Float] = List()
+
   def main(args: Array[String]) {
 
     // function to check if temperature is too hot or too cold
@@ -63,36 +71,104 @@ object StreamingJob {
       var message: String = ""
       val currentTime = System.currentTimeMillis() / 1000
 
+      /*
+        set the sensor timers to check sensor timeout
+        and add the temperature to the sensor list
+       */
       tuple.id match {
-        case "R1" => timerR1 = System.currentTimeMillis() / 1000
-        case "R2" => timerR2 = System.currentTimeMillis() / 1000
-        case "R3" => timerR3 = System.currentTimeMillis() / 1000
-        case "R4" => timerR4 = System.currentTimeMillis() / 1000
-        case "R5" => timerR5 = System.currentTimeMillis() / 1000
+        case "R1" => {
+          timerR1 = System.currentTimeMillis() / 1000
+          listR1 = tuple.temp.toFloat :: listR1
+        }
+        case "R2" => {
+          timerR2 = System.currentTimeMillis() / 1000
+          listR2 = tuple.temp.toFloat :: listR2
+        }
+        case "R3" => {
+          timerR3 = System.currentTimeMillis() / 1000
+          listR3 = tuple.temp.toFloat :: listR3
+        }
+        case "R4" => {
+          timerR4 = System.currentTimeMillis() / 1000
+          listR4 = tuple.temp.toFloat :: listR4
+        }
+        case "R5" => {
+          timerR5 = System.currentTimeMillis() / 1000
+          listR5 = tuple.temp.toFloat :: listR5
+        }
       }
 
-      if (timerR1 != 0 && currentTime - timerR1 > 5){
+      /*
+        wipe the sensor lists every 10 seconds
+       */
+      if (currentTime - wipeTimer > 10) {
+        listR1 = List()
+        listR2 = List()
+        listR3 = List()
+        listR4 = List()
+        listR5 = List()
+        wipeTimer = System.currentTimeMillis() / 1000
+      }
+
+      /*
+        if the temperature of a sensor was rising more than 2°C in the last 10 seconds
+        -> warning: room on fire
+       */
+      if (listR1.nonEmpty && listR1.head - listR1.min > 3) {
+        message = message + "R1: room on fire!" + " temperature is rising too fast" + "\n"
+      }
+      if (listR2.nonEmpty && listR2.head - listR2.min > 3) {
+        message = message + "R2: room on fire!" + " temperature is rising too fast" + "\n"
+      }
+      if (listR3.nonEmpty && listR3.head - listR3.min > 3) {
+        message = message + "R3: room on fire!" + " temperature is rising too fast" + "\n"
+      }
+      if (listR3.nonEmpty && listR4.head - listR4.min > 3) {
+        message = message + "R4: room on fire!" + " temperature is rising too fast" + "\n"
+      }
+      if (listR4.nonEmpty && listR5.head - listR5.min > 3) {
+        message = message + "R5: room on fire!" + " temperature is rising too fast" + "\n"
+      }
+
+      /*
+        if a sensor didn't sent data in the last 5 seconds
+        -> warning: sensor timeout
+      */
+      if (timerR1 != 0 && currentTime - timerR1 > 5) {
         message = message + "R1: timeout! please check if the sensor is working!" + "\n"
       }
-      if (timerR2 != 0 && currentTime - timerR2 > 5){
+      if (timerR2 != 0 && currentTime - timerR2 > 5) {
         message = message + "R2: timeout! please check if the sensor is working!" + "\n"
       }
-      if (timerR3 != 0 && currentTime - timerR3 > 5){
+      if (timerR3 != 0 && currentTime - timerR3 > 5) {
         message = message + "R3: timeout! please check if the sensor is working!" + "\n"
       }
-      if (timerR4 != 0 && currentTime - timerR4 > 5){
+      if (timerR4 != 0 && currentTime - timerR4 > 5) {
         message = message + "R4: timeout! please check if the sensor is working!" + "\n"
       }
-      if (timerR5 != 0 && currentTime - timerR5 > 5){
+      if (timerR5 != 0 && currentTime - timerR5 > 5) {
         message = message + "R5: timeout! please check if the sensor is working!" + "\n"
       }
 
+      /*
+        if the temperature of the sensor is higher than 60°C
+        -> warning: room on fire
+       */
       if (tuple.temp.toFloat > 60f) {
-        message = message + tuple.id + ": room on fire!" + " temperature: " + tuple.temp + "°C" + "\n"
+        message = message + tuple.id + ": room on fire!" + " temperature: " + tuple.temp + "°C" + "\n" + listR1.length.toString
       }
+
+      /*
+        if the temperature of the sensor is lower than 15°C
+        -> warning: close the windows
+       */
       if (tuple.temp.toFloat < 15f) {
         message = message + tuple.id + ": do not forget to close the windows!" + " temperature: " + tuple.temp + "°C" + "\n"
       }
+
+      /*
+        Return all warnings
+       */
       message
     }
 
