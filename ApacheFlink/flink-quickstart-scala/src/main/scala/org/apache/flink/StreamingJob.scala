04e0eb8ded6c23a4dd5220dab034e33e5d5cfeea
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-11-29 17:12:03 +0100

    WP6: Code optimization

--- ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
+++ ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
@@ -50,127 +50,85 @@ import scala.collection.TraversableOnce
 
 object StreamingJob {
 
-  var timerR1: Long = 0
-  var timerR2: Long = 0
-  var timerR3: Long = 0
-  var timerR4: Long = 0
-  var timerR5: Long = 0
-
+  var timerList : List[(String,Long)] = List()
+  var tempList1 : List[(String,Float)] = List()
+  var tempList2 : List[(String,Float)] = List()
   var wipeTimer: Long = System.currentTimeMillis() / 1000
+  var currentTime: Long = 0
+  var decideList: Boolean = true
 
-  var listR1: List[Float] = List()
-  var listR2: List[Float] = List()
-  var listR3: List[Float] = List()
-  var listR4: List[Float] = List()
-  var listR5: List[Float] = List()
+  def timeoutWriter(sensor: String, sensorTimer: Long, currentTime: Long): String = {
+    if (currentTime - sensorTimer > 5){
+      sensor + ": timeout! please check if the sensor is still working!" + "\n"
+    } else {
+      ""
+    }
+  }
+  def getTimeouts(list: List[(String, Long)], messages: String) : String = {
+    list match {
+      case Nil => messages
+      case x :: xs => getTimeouts(xs, messages + timeoutWriter(x._1, x._2, currentTime))
+    }
+  }
+
+  def checkTemp(tuple: roomTemp): String = {
+    var message: String = ""
+    currentTime = System.currentTimeMillis() / 1000
+
+    // add the current temperature to the templist
+    tempList1 = (tuple.id, tuple.temp.toFloat) :: tempList1
+    tempList2 = (tuple.id, tuple.temp.toFloat) :: tempList2
+
+    // if there is already an element for the current sensor in the list, remove it
+    // then add the current time for the sensor to the timerlist
+    timerList = timerList.filterNot(_._1 == tuple.id)
+    timerList = (tuple.id, System.currentTimeMillis() / 1000) :: timerList
+
+    // sensor timeout if a sensor didn't send any data in 5 seconds
+    message = getTimeouts(timerList, message)
+
+    // the room is on fire if the temperature is higher than 60°C
+    if (tuple.temp.toFloat > 60f) {
+      message = message + tuple.id + ": room on fire!" + " temperature: " + tuple.temp + "°C" + "\n"
+    }
+
+    // close the windows if the temperature is lower than 15°C
+    if (tuple.temp.toFloat < 15f) {
+      message = message + tuple.id + ": do not forget to close the windows!" + " temperature: " + tuple.temp + "°C" + "\n"
+    }
+
+    // first, remove all elements that are not relevant (data of other sensors)
+    // then compare the current temperature with the minimum temperature of a specific time period
+    // if the current temperature is more than 3°C higher, then the temperature rose too fast and the room is on fire
+    val filteredTempList1: List[Float] = tempList1.filter(_._1 == tuple.id).map(_._2)
+    val filteredTempList2: List[Float] = tempList2.filter(_._1 == tuple.id).map(_._2)
+    if (tuple.temp.toFloat - filteredTempList1.min > 3 || tuple.temp.toFloat - filteredTempList2.min > 3) {
+      message = message + tuple.id + ": room on fire!" + " temperature is rising too fast" + "\n"
+    }
+
+    // 2 lists with temperatures that are getting cleared in rotations
+    // lists:
+    // |--------------|--------------|--------------|--------------|
+    // |-------|--------------|--------------|--------------|-------
+    // clear:
+    // |-------|------|-------|------|-------|-------|------|------|
+    // if every 12 seconds one list is cleared, then the minimum covered time range is 12 seconds and the maximum 24 seconds
+    if (currentTime - wipeTimer > 12) {
+      if (decideList) {
+        tempList1 = List()
+        decideList = false
+      } else {
+        tempList2 = List()
+        decideList = true
+      }
+      wipeTimer = System.currentTimeMillis() / 1000
+    }
+
+    // return all warnings
+    message
+  }
 
   def main(args: Array[String]) {
-
-    // function to check if temperature is too hot or too cold
-    def checkTemp(tuple: roomTemp): String = {
-      var message: String = ""
-      val currentTime = System.currentTimeMillis() / 1000
-
-      /*
-        set the sensor timers to check sensor timeout
-        and add the temperature to the sensor list
-       */
-      tuple.id match {
-        case "R1" => {
-          timerR1 = System.currentTimeMillis() / 1000
-          listR1 = tuple.temp.toFloat :: listR1
-        }
-        case "R2" => {
-          timerR2 = System.currentTimeMillis() / 1000
-          listR2 = tuple.temp.toFloat :: listR2
-        }
-        case "R3" => {
-          timerR3 = System.currentTimeMillis() / 1000
-          listR3 = tuple.temp.toFloat :: listR3
-        }
-        case "R4" => {
-          timerR4 = System.currentTimeMillis() / 1000
-          listR4 = tuple.temp.toFloat :: listR4
-        }
-        case "R5" => {
-          timerR5 = System.currentTimeMillis() / 1000
-          listR5 = tuple.temp.toFloat :: listR5
-        }
-      }
-
-      /*
-        wipe the sensor lists every 10 seconds
-       */
-      if (currentTime - wipeTimer > 10) {
-        listR1 = List()
-        listR2 = List()
-        listR3 = List()
-        listR4 = List()
-        listR5 = List()
-        wipeTimer = System.currentTimeMillis() / 1000
-      }
-
-      /*
-        if the temperature of a sensor was rising more than 2°C in the last 10 seconds
-        -> warning: room on fire
-       */
-      if (listR1.nonEmpty && listR1.head - listR1.min > 3) {
-        message = message + "R1: room on fire!" + " temperature is rising too fast" + "\n"
-      }
-      if (listR2.nonEmpty && listR2.head - listR2.min > 3) {
-        message = message + "R2: room on fire!" + " temperature is rising too fast" + "\n"
-      }
-      if (listR3.nonEmpty && listR3.head - listR3.min > 3) {
-        message = message + "R3: room on fire!" + " temperature is rising too fast" + "\n"
-      }
-      if (listR3.nonEmpty && listR4.head - listR4.min > 3) {
-        message = message + "R4: room on fire!" + " temperature is rising too fast" + "\n"
-      }
-      if (listR4.nonEmpty && listR5.head - listR5.min > 3) {
-        message = message + "R5: room on fire!" + " temperature is rising too fast" + "\n"
-      }
-
-      /*
-        if a sensor didn't sent data in the last 5 seconds
-        -> warning: sensor timeout
-      */
-      if (timerR1 != 0 && currentTime - timerR1 > 5) {
-        message = message + "R1: timeout! please check if the sensor is working!" + "\n"
-      }
-      if (timerR2 != 0 && currentTime - timerR2 > 5) {
-        message = message + "R2: timeout! please check if the sensor is working!" + "\n"
-      }
-      if (timerR3 != 0 && currentTime - timerR3 > 5) {
-        message = message + "R3: timeout! please check if the sensor is working!" + "\n"
-      }
-      if (timerR4 != 0 && currentTime - timerR4 > 5) {
-        message = message + "R4: timeout! please check if the sensor is working!" + "\n"
-      }
-      if (timerR5 != 0 && currentTime - timerR5 > 5) {
-        message = message + "R5: timeout! please check if the sensor is working!" + "\n"
-      }
-
-      /*
-        if the temperature of the sensor is higher than 60°C
-        -> warning: room on fire
-       */
-      if (tuple.temp.toFloat > 60f) {
-        message = message + tuple.id + ": room on fire!" + " temperature: " + tuple.temp + "°C" + "\n" + listR1.length.toString
-      }
-
-      /*
-        if the temperature of the sensor is lower than 15°C
-        -> warning: close the windows
-       */
-      if (tuple.temp.toFloat < 15f) {
-        message = message + tuple.id + ": do not forget to close the windows!" + " temperature: " + tuple.temp + "°C" + "\n"
-      }
-
-      /*
-        Return all warnings
-       */
-      message
-    }
 
     // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
@@ -195,5 +153,3 @@ object StreamingJob {
   }
 
 }
-
-
