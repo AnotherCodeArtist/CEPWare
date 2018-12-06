be2b5d0e4e4b8a785b20b37dd4c5923d4a9985f0
Sebastian Weiland <hello@sweiland.at>
2018-12-06 12:05:44 +0100

    re-add nodred-javascript file
    (simplifies development a lot)

---
+++ Scripts/dataGenerator.js
@@ -0,0 +1,21 @@
+var count = context.get('count') || 0;
+count++;
+context.set('count', count);
+
+if (count <= 20) {
+    msg.payload = "t|20";
+} else if (count <= 50) {
+    msg.payload = "t|" + count;
+} else {
+    msg.payload = "t|" + Math.floor(Math.random() * (90 - 30) + 30);
+}
+
+msg.url = "http://172.17.0.8:7896/iot/d?k=test&i=IoT-R" + Math.floor(Math.random() * (6 - 1) + 1);
+msg.headers = {};
+msg.headers['Fiware-Service'] = 'cepware';
+msg.headers['Fiware-ServicePath'] = '/rooms';
+msg.headers['Content-Type'] = 'text/plain';
+
+node.warn(msg.payload);
+
+return msg;
