3de257c4082cadcad9755b2f726cacf6e18f637d
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-12-08 17:00:01 +0100

    WP6: Implementation of ProcessWindowFunction

--- ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
+++ ApacheFlink/flink-quickstart-scala/src/main/scala/org/apache/flink/StreamingJob.scala
@@ -33,14 +33,13 @@ import org.apache.flink.util.Collector
   * org.apache.flink.StreamingJob
   */
 
-/*object StreamingJob {
 
-  var timerList : List[(String,Long)] = List()
-  var tempList1 : List[(String,Float)] = List()
-  var tempList2 : List[(String,Float)] = List()
-  var wipeTimer: Long = System.currentTimeMillis() / 1000
+object StreamingJob {
+
+  var timerList: List[(String, Long)] = List()
   var currentTime: Long = 0
-  var decideList: Boolean = true
+  var timeChecker: Long = 0
+  var timeOuts: String = ""
 
   def timeoutWriter(sensor: String, sensorTimer: Long, currentTime: Long): String = {
     if (currentTime - sensorTimer > 5){
@@ -56,94 +55,18 @@ import org.apache.flink.util.Collector
     }
   }
 
-  def checkTemp(tuple: roomTemp): String = {
-    var message: String = ""
+  def main(args: Array[String]) {
+
     currentTime = System.currentTimeMillis() / 1000
-
-    // add the current temperature to the templist
-    tempList1 = (tuple.id, tuple.temp.toFloat) :: tempList1
-    tempList2 = (tuple.id, tuple.temp.toFloat) :: tempList2
-
-    // if there is already an element for the current sensor in the list, remove it
-    // then add the current time for the sensor to the timerlist
-    timerList = timerList.filterNot(_._1 == tuple.id)
-    timerList = (tuple.id, System.currentTimeMillis() / 1000) :: timerList
-
-    // sensor timeout if a sensor didn't send any data in 5 seconds
-    message = getTimeouts(timerList, message)
-
-    // the room is on fire if the temperature is higher than 60°C
-    if (tuple.temp.toFloat > 60f) {
-      message = message + tuple.id + ": room on fire!" + " temperature: " + tuple.temp + "°C" + "\n"
-    }
-
-    // close the windows if the temperature is lower than 15°C
-    if (tuple.temp.toFloat < 15f) {
-      message = message + tuple.id + ": do not forget to close the windows!" + " temperature: " + tuple.temp + "°C" + "\n"
-    }
-
-    // first, remove all elements that are not relevant (data of other sensors)
-    // then compare the current temperature with the minimum temperature of a specific time period
-    // if the current temperature is more than 3°C higher, then the temperature rose too fast and the room is on fire
-    val filteredTempList1: List[Float] = tempList1.filter(_._1 == tuple.id).map(_._2)
-    val filteredTempList2: List[Float] = tempList2.filter(_._1 == tuple.id).map(_._2)
-    if (tuple.temp.toFloat - filteredTempList1.min > 3 || tuple.temp.toFloat - filteredTempList2.min > 3) {
-      message = message + tuple.id + ": room on fire!" + " temperature is rising too fast" + "\n"
-    }
-
-    // 2 lists with temperatures that are getting cleared in rotations
-    // lists:
-    // |--------------|--------------|--------------|--------------|
-    // |-------|--------------|--------------|--------------|-------
-    // clear:
-    // |-------|------|-------|------|-------|-------|------|------|
-    // if every 12 seconds one list is cleared, then the minimum covered time range is 12 seconds and the maximum 24 seconds
-    if (currentTime - wipeTimer > 12) {
-      if (decideList) {
-        tempList1 = List()
-        decideList = false
-      } else {
-        tempList2 = List()
-        decideList = true
-      }
-      wipeTimer = System.currentTimeMillis() / 1000
-    }
-
-    // return all warnings
-    message
-  }
-
-  def main(args: Array[String]) {
-
-    // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val eventStream = env.addSource(new OrionSource(9001))
 
-    val processedDataStream = eventStream.flatMap(event => event.entities)
-      .map(entity => new roomTemp(entity.id, entity.attrs("temperature").value.asInstanceOf[String]))
-
-    val warningStream = processedDataStream.map(checkTemp(_))
-    val filteredWarningStream = warningStream.filter(_ != "")
-
-
-    // Writing the Results in a log.txt File
-    // Attention: log.txt must not exist before the task is executed
-    filteredWarningStream.writeAsText("/tmp/log.txt")
-
-    // execute program
-    env.execute("Socket Window NgsiEvent")
-  }
-
-  class roomTemp(var id: String, var temp: String) {
-  }
-
-}*/
-
-object StreamingJob {
-  def main(args: Array[String]) {
-
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val eventStream = env.addSource(new OrionSource(9001))
+    if (currentTime - timeChecker > 5) {
+      timeOuts = getTimeouts(timerList, "")
+      timeChecker = System.currentTimeMillis() / 1000
+    } else {
+      timeOuts = ""
+    }
 
     val processedDataStream = eventStream.flatMap(event => event.entities)
       .map(entity => (entity.id, entity.attrs("temperature").value.asInstanceOf[String]))
@@ -151,17 +74,38 @@ object StreamingJob {
       .window(TumblingProcessingTimeWindows.of(Time.seconds(1)))
       .process(new MyProcessWindowFunction)
 
+
+
+    processedDataStream.writeAsText("/tmp/log.txt")
+
     env.execute("Socket Window NgsiEvent")
   }
 
   class MyProcessWindowFunction extends ProcessWindowFunction[(String, String), String, String, TimeWindow] {
 
     override def process(key: String, context: Context, elements: Iterable[(String, String)], out: Collector[String]): Unit = {
-      var count: Int = 0
-      for (in <- elements) {
-        count = count + 1
+      var message: String = ""
+      val values: Iterable[Float] = elements.map(_._2.toFloat)
+      val max: Float = values.max
+      val min: Float = values.min
+
+      timerList = timerList.filterNot(_._1 == key)
+      timerList = (key, System.currentTimeMillis() / 1000) :: timerList
+
+      if (max >= 60){
+        message = message + message + key + ": room on fire!" + " temperature: " + max.toString + "°C" + "\n"
       }
-      out.collect(s"Window ${context.window} count: $count")
+      if (min <= 15) {
+        message = message + key + ": do not forget to close the windows!" + " temperature: " + min.toString + "°C" + "\n"
+      }
+      if (max - min >= 3) {
+        message = message + key + ": room on fire!" + " temperature is rising too fast" + "\n"
+      }
+      timerList = timerList
+
+      message = message + timeOuts
+
+      out.collect(message)
     }
   }
 
