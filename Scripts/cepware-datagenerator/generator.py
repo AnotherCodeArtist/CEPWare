e77d5bbad04c0b287bd640b12d703bceaf1b19ae
Sebastian Weiland <hello@sweiland.at>
2018-12-08 04:08:27 +0100

    next improvement: read generation strategy/test-mode from command line arguments

--- Scripts/cepware-datagenerator/generator.py
+++ Scripts/cepware-datagenerator/generator.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 import requests
 
@@ -11,25 +12,36 @@ class Generator:
         self.url = ""
         self.headers = {}
         self.count = counter.Counter()
+        if sys.argv[1] == "--strategy":
+            self.mode = sys.argv[2]
+        else:
+            self.mode = "normal"
 
     def main(self):
+        self.generate(self.mode)
+
+    def generate(self, strategy):
         self.count.start()
+        while True:
+            if strategy == "normal":
+                self.payload = "t|20"
+                self.send_out(self.payload)
+                print(self.payload)
+            elif strategy == "fire":
+                self.payload = "t|" + str(self.count.peek() + 20)
+                self.send_out(self.payload)
+                print(self.payload)
+            elif strategy == "failure":
+                self.payload = "t|" + str(random.randint(30, 90))
+                self.send_out(self.payload)
+                print(self.payload)
 
-        while self.count.peek() <= 20:
-            self.payload = "t|20"
-            print(self.payload)
-        while self.count.peek() <= 50:
-            self.payload = "t|" + str(self.count.peek())
-            print(self.payload)
-        while self.count.peek() <= 120:
-            self.payload = "t|" + str(random.randint(30, 90))
-            print(self.payload)
-
+    def send_out(self, payload):
         # self.url = "http://localhost:7896/iot/d?k=test&i=IoT-R" + str(random.randint(0, 5))
         self.url = "https://httpbin.org/post"
         self.headers = {"Fiware-Service": "cepware", "Fiware-ServicePath": "/rooms", "Content-Type": "text/plain"}
 
-        requests.post(self.url, headers=self.headers, data=self.payload)
+        requests.post(self.url, headers=self.headers, data=payload)
 
 
 g = Generator()
