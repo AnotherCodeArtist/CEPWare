aeccd4ac09b67ff143df5419e87f98a1b46c792e
Sebastian Weiland <hello@sweiland.at>
2018-12-08 02:35:24 +0100

    first improvement: use while instead of if-elif-else-jungle
    also spam terminal with numbers for now

--- Scripts/cepware-datagenerator/generator.py
+++ Scripts/cepware-datagenerator/generator.py
@@ -1,24 +1,36 @@
-import counter
-import time
 import random
+
 import requests
 
-payload = ""
+import counter
 
-count = counter.Counter()
-count.start()
 
-if count.peek() <= 20:
-    payload = "t|20"
-elif count.peek() <= 50:
-    payload = "t|" + str(count.peek())
-else:
-    payload = "t|" + str(random.randint(30, 90))
+class Generator:
+    def __init__(self):
+        self.payload = ""
+        self.url = ""
+        self.headers = {}
+        self.count = counter.Counter()
 
-url = "http://localhost:7896/iot/d?k=test&i=IoT-R" + str(random.randint(0, 5))
-headers = {"Fiware-Service": "cepware", "Fiware-ServicePath": "/rooms", "Content-Type": "text/plain"}
+    def main(self):
+        self.count.start()
 
-r = requests.post(url, headers=headers, data=payload)
+        while self.count.peek() <= 20:
+            self.payload = "t|20"
+            print(self.payload)
+        while self.count.peek() <= 50:
+            self.payload = "t|" + str(self.count.peek())
+            print(self.payload)
+        while self.count.peek() <= 120:
+            self.payload = "t|" + str(random.randint(30, 90))
+            print(self.payload)
 
-time.sleep(5)
-print(payload)
+        # self.url = "http://localhost:7896/iot/d?k=test&i=IoT-R" + str(random.randint(0, 5))
+        self.url = "https://httpbin.org/post"
+        self.headers = {"Fiware-Service": "cepware", "Fiware-ServicePath": "/rooms", "Content-Type": "text/plain"}
+
+        requests.post(self.url, headers=self.headers, data=self.payload)
+
+
+g = Generator()
+g.main()
