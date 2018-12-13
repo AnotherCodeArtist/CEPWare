259ac5d73b32fc88bb94da6cc0a1fab2ed4955c6
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2018-12-13 19:05:49 +0100

    WP6: Commentary

--- cepware/src/main/scala/cep/StreamingJob.scala
+++ cepware/src/main/scala/cep/StreamingJob.scala
@@ -18,12 +18,13 @@
 
 package cep
 
+import org.apache.flink.core.fs.FileSystem
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.fiware.cosmos.orion.flink.connector.OrionSource
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
-import org.apache.flink.streaming.api.windowing.assigners.{SlidingProcessingTimeWindows}
+import org.apache.flink.streaming.api.windowing.assigners.SlidingProcessingTimeWindows
 import org.apache.flink.util.Collector
 
 
@@ -37,16 +38,19 @@ import org.apache.flink.util.Collector
 object StreamingJob {
 
   var currentTime: Long = 0
-  var timeChecker: Long = 0
   var timerMap: Map[String,Long] = Map()
 
+  // function to find not working sensors and write hint messages
   def timeoutWriter(sensor: String, sensorTimer: Long, currentTime: Long): String = {
-    if (currentTime - sensorTimer > 5){
+    if (currentTime - sensorTimer > 10){
       sensor + ": timeout! please check if the sensor is still working!" + "\n"
     } else {
       ""
     }
   }
+
+  // function to iterate over the map timerMap in order to find sensors that aren't working anymore by applying the
+  // timeoutWriter function
   def getTimeouts(map: Map[String, Long], message: String): String = {
     var text: String = message
     map foreach (x => text += timeoutWriter(x._1, x._2, currentTime))
@@ -61,9 +65,12 @@ object StreamingJob {
     val processedDataStream = eventStream.flatMap(event => event.entities)
       .map(entity => (entity.id, entity.attrs("temperature").value.asInstanceOf[String]))
       .keyBy(_._1)
-      .window(SlidingProcessingTimeWindows.of(Time.seconds(20), Time.seconds(5)))
+      .window(SlidingProcessingTimeWindows.of(Time.seconds(30), Time.seconds(15)))
       .process(new MyProcessWindowFunction)
-      .writeAsText("/tmp/log.txt")
+      .writeAsText("/tmp/log.txt", FileSystem.WriteMode.OVERWRITE)
+    // option overwrite to be able to overwrite the log file
+    // otherwise the job would fail to start if a log file already exists
+    // attention: restarting the job clears the log file
 
     env.execute("Socket Window NgsiEvent")
   }
@@ -80,8 +87,12 @@ object StreamingJob {
       val max: Float = values.max
       val min: Float = values.min
 
+      // save the current time for the current sensor in order to be able to check if the sensor is not working anymore
+      // in the future
       timerMap += (key -> currentTime)
 
+      // checking if the temperature is too low (window not closed), too high (room burning) or if the temperature
+      // is rising too fast (also room burning)
       if (max >= 60){
         messages += key + ": room on fire!" + " temperature: " + max.toString + "°C" + "\n"
       }
@@ -92,6 +103,7 @@ object StreamingJob {
         messages += key + ": room on fire!" + " temperature is rising too fast" + "\n"
       }
 
+      // searching for timeouts and deleting the last carriage return
       messages = getTimeouts(timerMap, messages).stripLineEnd
       out.collect(messages)
     }
