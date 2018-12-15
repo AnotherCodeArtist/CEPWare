c35c222af1168a57ee96f0a4468f1facc0eaeb41
GregorFernbach <gregor.fernbach.2@mailbox.org>
2018-12-15 20:43:14 +0100

    WP7: Adapting Setup script and cleaning up

--- Setup/Setup.py
+++ Setup/Setup.py
@@ -7,31 +7,40 @@ import tarfile
 
 import docker
 
+# Get the OS platform
 pltfrm = platform.system()
 print(
     "Welcome to CEPWARE. This is the automated setup. Before you run this script. Please do a docker-compose up. "
     "Please follow the instructions and be patient until everything is setup")
+
 input_var = input(
     "Please Enter the Full Path to your CEPWARE folder. \n Under Windows it could e.g. be C:\\Workspace\\CEPWARE, "
-    "here we need C:\\Workspace)")
+    "here we need C:\\Workspace)\n")
+print("Do you also want to start the automated data script ?")
+dataGeneration = input("For yes enter 'y' otherwise 'n'.")
+if(dataGeneration == "y"):
+    strategy = input("Please enter the strategy you want to simulate later. (fire, failure, normal)\n.")
+    location = input("Please specify where the data should be sent to. (local, fh, test)\n")
 
-strategy = input("Please enter the strategy you want to simulate later. (fire, failure, normal)\n.")
-location = input("Please specify where the data should be sent to. (local, fh, test)\n")
+# Based on the OS we have different seperators
 if pltfrm == "Windows":
     # input_var = input_var.split(os.sep)
     slash = os.sep
 else:
     # input_var = input_var.split(os.altsep)
     slash = os.altsep
-source = os.path.join(input_var, "CEPware", "cepware", "target",
-                      "cepware-1.5.jar")
-client = docker.from_env()
 
+source = os.path.join(input_var, "CEPware", "cepware", "target", "cepware-1.5.jar")
+
+# Get the container id of the jobmanager
+client = docker.from_env()
 container = client.containers.get("jobmanager")
 containerId = container.id
 destination = (containerId + ":" + "/opt/flink/cepware-1.5.jar")
 
+
 # Function to copy the Apache Flink Jar to the Taskmanager (Docker Apache FLink Container)
+# First we generate a tar file of the jar file which we copy into the container
 def copy_to(src, dst):
     name, dst = dst.split(':')
 
@@ -47,6 +56,7 @@ def copy_to(src, dst):
     data = open(src + '.tar', 'rb').read()
     container.put_archive(os.path.dirname(dst), data)
 
+
 print("Starting Apache Flink. Please be patient...")
 copy_to(source, destination)
 # Execute the apache flink jar on the taskmanager
@@ -54,38 +64,38 @@ container.exec_run(cmd="flink run -d ./cepware-1.5.jar", workdir="/opt/flink/")
 print("Apache Flink is up and running.")
 
 print("Setting up the infrastructure and sending requests. Please be patient...")
-#Call the requests
+# Call the requests
 # Setup the Subscription from ORION to Cygnus
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "Subscriptions", "post_subscription_cygnus.py"))])
-# Setup the Subscription from ORION to Apache Flink
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "Subscriptions", "post_subscription_flink.py"))])
-# Create the ORION entities for Room 1 - 5
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "ORION", "post_create_R1.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "ORION", "post_create_R2.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "ORION", "post_create_R3.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "ORION", "post_create_R4.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "ORION", "post_create_R5.py"))])
-# Register IDAS service group
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "IDAS", "post_provision_service_group.py"))])
-# Register IDAS entities for Room 1 -5
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "IDAS", "post_register_IoT-R1.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "IDAS", "post_register_IoT-R2.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "IDAS", "post_register_IoT-R3.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "IDAS", "post_register_IoT-R4.py"))])
-subprocess.call(["python", (os.path.join(input_var, "CEPware", "Scripts", "IDAS", "post_register_IoT-R5.py"))])
+
+requests = [os.path.join("Subscriptions", "post_subscription_cygnus.py"),
+            os.path.join("Subscriptions", "post_subscription_flink.py"),
+            os.path.join("ORION", "post_create_R1.py"), os.path.join("ORION", "post_create_R2.py"),
+            os.path.join("ORION", "post_create_R3.py"), os.path.join("ORION", "post_create_R4.py"),
+            os.path.join("ORION", "post_create_R5.py"), os.path.join("IDAS", "post_provision_service_group.py"),
+            os.path.join("IDAS", "post_register_IoT-R1.py"),
+            os.path.join("IDAS", "post_register_IoT-R2.py"), os.path.join("IDAS", "post_register_IoT-R3.py"),
+            os.path.join("IDAS", "post_register_IoT-R4.py"), os.path.join("IDAS", "post_register_IoT-R5.py")]
+
+for req in requests:
+    subprocess.call(["python", (
+         os.path.join(input_var, "CEPware", "Setup", "Setup-Requests", req))])
+
 print("The infrastructure is up and running.")
 
-print("Starting Datageneration now! Exit with ctrl-c!")
-generator_path = os.path.join(input_var, "CEPware", "Scripts", "cepware-datagenerator-go", "")
+if dataGeneration == "y":
+    print("Starting Datageneration now! Exit with ctrl-c!")
+    generator_path = os.path.join(input_var, "CEPware", "Setup", "data-generator", "")
 
-if pltfrm == "Windows":
-    subprocess.run([generator_path + "cepware-datagenerator-go.exe", "-strategy=" + strategy,
-                    "-location=" + location])
-elif pltfrm == "Darwin":
-    subprocess.run([generator_path + "cepware-datagenerator-go-darwin", "-strategy=" + strategy,
-                    "-location=" + location])
+    if pltfrm == "Windows":
+        subprocess.run([generator_path + "cepware-datagenerator-go.exe", "-strategy=" + strategy,
+                        "-location=" + location])
+    elif pltfrm == "Darwin":
+        subprocess.run([generator_path + "cepware-datagenerator-go-darwin", "-strategy=" + strategy,
+                        "-location=" + location])
+    else:
+        subprocess.run([generator_path + "cepware-datagenerator-go-linux", "-strategy=" + strategy,
+                        "-location=" + location])
+
+    print("The whole system is up and running. You will now see the Data begin sent.")
 else:
-    subprocess.run([generator_path + "cepware-datagenerator-go-linux", "-strategy=" + strategy,
-                    "-location=" + location])
-
-print("The whole system is up and running. You will now see the Data begin sent.")
+    print("The system is set up and the automated Data script has not been started.")
---
+++ Setup/data-generator/generator2.go
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2018 CEPWare-Team (GregorFernbach, heiderst16, sweiland)
+ * For licensing information see LICENSE
+ */
+
+// package for data-generator for CEPWare
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	// define command line flags and add default values
+	strategy := flag.String("strategy", "normal", "generation strategy for data: normal, fire, failure, test")
+	flag.Parse()
+
+	// react to "location" flag
+	var url [5]string
+	switch *location {
+	
+	case "local":
+		url[0] = "http://localhost:7896/iot/d?k=test&i=IoT-R1"
+		url[1] = "http://localhost:7896/iot/d?k=test&i=IoT-R2"
+		url[2] = "http://localhost:7896/iot/d?k=test&i=IoT-R3"
+		url[3] = "http://localhost:7896/iot/d?k=test&i=IoT-R4"
+		url[4] = "http://localhost:7896/iot/d?k=test&i=IoT-R5"
+	}
+
+	// react to "room" and "strategy" flags
+	GenerateData(url, *strategy)
+}
+
+// function that generates data in UL2.0 format based on a strategy
+func GenerateData(url [5]string, strategy string) {
+	switch strategy {
+	case "normal":
+		payload := "t|20"
+		for {
+			for r := 0; r < 5; r++ {
+				MakeRequest(url[r], payload)
+			}
+		}
+	case "fire":
+		for i := 20; i < 50; i++ {
+			payload := "t|" + strconv.Itoa(i)
+			for r := 0; r < 5; r++ {
+				MakeRequest(url[r], payload)
+			}
+		}
+		for {
+			payload := "t|50"
+			for r := 0; r < 5; r++ {
+				MakeRequest(url[r], payload)
+			}
+		}
+	case "failure":
+		for i := 20; i < 100; i++ {
+			payload := "t|" + strconv.Itoa(rand.Intn(i))
+			for r := 0; r < 5; r++ {
+				MakeRequest(url[r], payload)
+			}
+		}
+		for {
+			payload := "t|" + strconv.Itoa(rand.Intn(100))
+			for r := 0; r < 5; r++ {
+				MakeRequest(url[r], payload)
+			}
+		}
+	}
+}
+
+// function that sends out data to an URL
+func MakeRequest(url string, payload string) {
+	reader := strings.NewReader(payload)
+
+	req, err := http.NewRequest("POST", url, reader)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Set("Fiware-Service", "cepware")
+	req.Header.Set("Content-Type", "text/plain")
+	req.Header.Set("Fiware-ServicePath", "/rooms")
+
+	client := &http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Sent '" + payload + "' to " + url)
+	time.Sleep(200 * time.Millisecond)
+}
